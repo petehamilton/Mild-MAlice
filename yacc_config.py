1fae74315b18e66c5e84c6de841bf7f80d981caa
Peter Hamilton <peter@inspiredpixel.net>
2011-10-25 13:27:10 +0100

    Initial attempt at yacc config file

---
+++ yacc_config.py
@@ -0,0 +1,91 @@
+def p_statement_list_alicefound(p):
+    'statement_list : PRINT_ALICE PRINT_SPOKE expression SEP_PERIOD'
+
+def p_statement_list_sep_comma(p):
+    'statement_list : statement SEP_COMMA statement_list'
+
+def p_statement_list_sep_period(p):
+    'statement_list : statement SEP_PERIOD statement_list'
+
+def p_statement_list_sep_and(p):
+    'statement_list : statement SEP_AND statement_list'
+
+def p_statement_list_sep_but(p):
+    'statement_list : statement SEP_BUT statement_list'
+
+def p_statement_list_sep_then(p):
+    'statement_list : statement SEP_THEN statement_list'
+
+def p_statement_too(p):
+    'statement : statement TOO'
+
+def p_statement_wasa(p):
+    'statement : ID DEC_WAS DEC_A type'
+
+def p_statement_became(p):
+    'statement : ID ASSIGNMENT expression'
+
+def p_type_number(p):
+    'type : TYPE_NUMBER'
+
+def p_type_letter(p):
+    'type : TYPE_LETTER'
+
+def p_expression_factor(p):
+    'expression : factor'
+
+def p_expression_not(p):
+    'expression : B_NOT expression'
+
+def p_expression_drank(p):
+    'expression : ID DECREMENT'
+
+def p_expression_ate(p):
+    'expression : ID INCREMENT'
+
+def p_expression_pipe(p):
+    'expression : expression B_OR term1'
+
+def p_expression_hat(p):
+    'expression : expression B_XOR term1'
+
+def p_expression_ampersand(p):
+    'expression : expression B_AND term1'
+
+def p_expression_term1(p):
+    'expression : term1'
+
+def p_term1_plus(p):
+    'term1 : term1 PLUS term2'
+    p[0] = p[1] + p[3]
+
+def p_term1_minus(p):
+    'term1 : term1 MINUS term2'
+
+def p_term1_term2(p):
+    'term1 : term2'
+
+def p_term2_multiply(p):
+    'term2 : term2 MULTIPLY factor'
+
+def p_term2_divide(p):
+    'term2 : term2 DIVIDE factor'
+
+def p_term2_factor(p):
+    'term2 : factor'
+
+def p_factor_parentheses(p):
+    'term2 : LPAREN expression RPAREN'
+
+def p_factor_number(p):
+    'factor : NUMBER'
+
+def p_factor_letter(p):
+    'factor : LETTER'
+
+def p_factor_id(p):
+    'factor : ID'
+
+# Error rule for syntax errors
+def p_error(p):
+    print "Syntax error in input!"
