b31d77316c92c35a65a04de1fc3702f39a524617
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-08 09:33:38 +0000

    Think that I have created a vaguely correct weight function

--- code_generator.py
+++ code_generator.py
@@ -2,16 +2,45 @@ def generate( node ):
 
 
 
-#def transExp( 
+def transExp( node, registers ):
+    if node.tokType == "binary_op":
+        if weight( node.children[1] ) > weight( node.children[2] ):
+            transExp( node.children[1], 
+        else:
+        
 
 
+
+# Node types are:
+# statement_list, spoke, assignment, declaration, 
+# binary_op, unary_op, type, factor
+#
+#
+#
+#
 def weight( node ):
     if node.tokType == "factor":
         return 1
 
-    elif node.tokType == "binary_op"
+    elif node.tokType == "binary_op":
         cost1 = max( weight(node.children[1]), weight(node.children[2]) + 1 )
         cost2 = max( weight(node.children[2]), weight(node.children[1]) + 1 )
-        return min( 
+        return min( cost1, cost2 )
 
-    elif node.tokType == "unary_op"
+    elif node.tokType == "unary_op":
+        return weight(node.children[1])
+
+    elif node.tokType == "spoke":
+        return weight(node.children[1])
+
+    elif node.tokType == "assignment":
+
+    elif node.tokType == "statement_list":
+        return max( weight(node.children[0]), weight(node.children[1]) )
+
+    #is this right? Dont need to store anything in register yet
+    elif node.tokType == "declaration" or node.tokType == "type:
+        pass
+
+
+
