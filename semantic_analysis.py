3e8a041a57981c5521f1ed7fe073c86cbc8126b2
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-12 12:28:14 +0000

    Accidentally removed a line of code. added back in

--- semantic_analysis.py
+++ semantic_analysis.py
@@ -53,4 +53,5 @@ def analyse( symbolTable, node ):
         if node.children[0] in symbolTable:
             raise SemanticException( node.lineno, node.clauseno, "You already told me what '%s' was on line %d" %(node.children[0],  symbolTable[node.children[0]][1]) )
         else:    
+            symbolTable[node.children[0]] = [node.children[1].children[0], node.lineno, True]  
 
