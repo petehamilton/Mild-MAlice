6ed9c2b5156ae116b5ccf40f4ee30f72baec035d
Peter Hamilton <peter@inspiredpixel.net>
2011-11-14 20:37:25 +0000

    Edited test.sh so it produces the same output as the given example output

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 for f in milestone2/*.alice 
 do
-    echo "Compiling $f"
+    fname=`basename $f`
+    echo "Compiling $fname"
     python main.py $f
     if [ -f output.asm ]
     then
-    echo "Running $f" 
+    echo "Running $fname" 
     nasm -f elf64 output.asm ; gcc -m64 -o output output.o ; ./output
     rm output.asm output.o
     fi
