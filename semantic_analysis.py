9bc8c5b5ab2694788c2b77242788571ad4aa4b1f
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-15 09:44:29 +0000

    Changed semantic analysis to include types from Node instead of matching against strings

--- semantic_analysis.py
+++ semantic_analysis.py
@@ -1,46 +1,48 @@
 import sys
+import Node as n
 
 from grammar_exceptions import SemanticException
 
-def analyse( symbolTable, node ):
-    if node.tokType == "statement_list":
-        analyse( symbolTable, node.children[0] )
-        analyse( symbolTable, node.children[1] )
+def analyse( symbolTable, node, flags ):
+    if node.tokType == n.STATEMENT_LIST:
+        analyse( symbolTable, node.children[0], flags  )
+        analyse( symbolTable, node.children[1], flags  )
 
-    elif node.tokType == "spoke":
-        type1 = analyse( symbolTable, node.children[0])
+    elif node.tokType == n.SPOKE:
+        type1 = analyse( symbolTable, node.children[0], flags )
         if type1[0] == 'ID':
             (idType, lineNo, assigned ) = symbolTable[node.children[0]]
+            flags[n.SPOKE].append( idType )
             if not assigned:
                 raise SemanticException( node.lineno, node.clauseno)
 
-    elif node.tokType == "assignment":
+    elif node.tokType == n.ASSIGNMENT:
         (identifier, expression) = node.children
-        type1 = analyse( symbolTable, expression )
+        type1 = analyse( symbolTable, expression, flags  )
         if type1 == symbolTable[identifier][0]:
             symbolTable[identifier][2] = True
         else:
             raise SemanticException( node.lineno, node.clauseno)
 
-    elif node.tokType == "type":
+    elif node.tokType == n.TYPE:
         return node.children[0]
         
-    elif node.tokType == "unary_op":
-        type1 = analyse( symbolTable, node.children[1])
-        if type1 == "number":
-            return "number"
+    elif node.tokType == n.UNARY_OP:
+        type1 = analyse( symbolTable, node.children[1], flags )
+        if type1 == n.NUMBER:
+            return n.NUMBER
         else:
             raise SemanticException( node.lineno, node.clauseno)
     
-    elif node.tokType == "binary_op":
-        type1 = analyse( symbolTable, node.children[1])
-        type2 = analyse( symbolTable, node.children[2])
-        if type1 == type2 == "number":
-            return "number"
+    elif node.tokType == n.BINARY_OP:
+        type1 = analyse( symbolTable, node.children[1], flags )
+        type2 = analyse( symbolTable, node.children[2], flags )
+        if type1 == type2 == n.NUMBER:
+            return n.NUMBER
         else:
             raise SemanticException( node.lineno, node.clauseno)
 
-    elif node.tokType == "factor":
+    elif node.tokType == n.FACTOR:
         if node.children[0] == 'ID':
             (idType, lineNo, assigned ) = symbolTable[node.children[1]]
             if assigned:
@@ -49,7 +51,7 @@ def analyse( symbolTable, node ):
                 raise SemanticException( node.lineno, node.clauseno)
         return node.children[0]
         
-    elif node.tokType == "declaration":
+    elif node.tokType == n.DECLARATION:
         if node.children[0] in symbolTable:
             raise SemanticException( node.lineno, node.clauseno, "You already told me what '%s' was on line %d" %(node.children[0],  symbolTable[node.children[0]][1]) )
         else:    
