00ed7a430d676fb01de63ef5959b2814622c9f56
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-16 21:24:31 +0000

    Restructured Node inheritance to include a non instruction class, for the case of spoke.

--- ImmediateNodes.py
+++ ImmediateNodes.py
@@ -1,29 +1,34 @@
 class IntermediateNode(object):
-    def __init__(self, instruction):
-        self.instruction = instruction
-    
+    def __init__(self):
+        pass
     #TODO: MAKE THIS ABSTRACT
     def generateCode():
         pass
+
+class InstructionNode(object):
+    def __init__(self, instruction):
+        super(InstructionNode, self).__init__()
+        self.instruction = instruction
+          
         
-class MovNode(IntermediateNode):
+class MovNode(InstructionNode):
     def __init__(self, reg1, reg2):
         super(MovNode, self).__init__("mov")
         self.reg1 = reg1
         self.reg2 = reg2
     
-class ImmMovNode(IntermediateNode):  
+class ImmMovNode(InstructionNode):  
     def __init__(self, reg, immediateValue):
         super(ImmMovNode, self).__init__("imov")  
         self.reg = reg
         self.imm = immediateValue
     
 #TODO MAKE THIS ABSTRACT
-class BinOpNode(IntermediateNode):
+class BinOpNode(InstructionNode):
     def __init__(self, instruction, reg1, reg2):
         super(BinOpNode, self).__init__(instruction)  
-        this.reg1 = reg1
-        this.reg2 = reg2
+        self.reg1 = reg1
+        self.reg2 = reg2
 
  
 class AddNode(BinOpNode):
@@ -34,13 +39,13 @@ class SubNode(BinOpNode):
     def __init__(self, reg1, reg2):
         super(SubNode, self).__init__("sub", reg1, reg2) 
          
-class ImulNode(BinOpNode):
+class MulNode(BinOpNode):
     def __init__(self, reg1, reg2):
-        super(ImulNode, self).__init__("imul", reg1, reg2)
+        super(MulNode, self).__init__("imul", reg1, reg2)
           
-class IdivNode(BinOpNode):
+class DivNode(BinOpNode):
     def __init__(self, reg1, reg2):
-        super(IdivNode, self).__init__("idiv", reg1, reg2)  
+        super(DivNode, self).__init__("idiv", reg1, reg2)  
         
 class ModNode(BinOpNode):
     def __init__(self, reg1, reg2):
@@ -50,18 +55,18 @@ class OrNode(BinOpNode):
     def __init__(self, reg1, reg2):
         super(OrNode, self).__init__("or", reg1, reg2)  
         
-class XorNode(BinOpNode):
+class XORNode(BinOpNode):
     def __init__(self, reg1, reg2):
-        super(XorNode, self).__init__("xor", reg1, reg2) 
+        super(XORNode, self).__init__("xor", reg1, reg2) 
          
 class AndNode(BinOpNode):
     def __init__(self, reg1, reg2):
         super(AndNode, self).__init__("and", reg1, reg2)  
 
-class UnOpNode(IntermediateNode):
+class UnOpNode(InstructionNode):
     def __init__(self, instruction, reg):
         super(UnOpNode, self).__init__(instruction)  
-        this.reg = reg
+        self.reg = reg
         
 class IncNode(UnOpNode):
     def __init__(self, reg):
@@ -74,5 +79,9 @@ class DecNode(UnOpNode):
 class NotNode(UnOpNode):
     def __init__(self, reg):
         super(NotNode, self).__init__("not", reg)
-    
+
+class SpokeNode(ImmediateNode):
+    def __init__(self, reg):
+        super(SpokeNode, self).__init__(reg)
+        self.reg = reg
     
