8d07fac3863d6256e9a4e1ec21ef7b005fc6d938
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-20 10:38:10 +0000

    Renamed Node.py to ASTNode.py and fixed all imports in other files. Removed unused global variables in yacc_config.

--- yacc_config.py
+++ yacc_config.py
@@ -1,18 +1,6 @@
-import Node as n
-from Node import Node
+import ASTNode
 from tokrules import tokens
 import grammar_exceptions as e
-
-UNARY_OP = "unary_op"
-BINARY_OP = "binary_op"
-FACTOR = "factor"
-ASSIGNMENT = "assignment"
-DECLARATION = "declaration"
-SPOKE = "spoke"
-STATEMENT_LIST = "statement_list"
-TYPE = "type"
-
-
 
 start = 'statement_list' # Optional as uses first rule
 class ParseError(Exception): 
@@ -33,11 +21,11 @@ def _parse_error(msg, coord):
 
 def p_statement_list_sep_comma(p):
     'statement_list : statement SEP_COMMA statement_list'
-    p[0] = StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_list_sep_period(p):
     'statement_list : statement SEP_PERIOD statement_list'
-    p[0] = StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_list_statement(p):
     'statement_list : statement SEP_PERIOD'
@@ -45,19 +33,19 @@ def p_statement_list_statement(p):
 
 def p_statement_list_sep_and(p):
     'statement_list : statement SEP_AND statement_list'
-    p[0] = StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_list_sep_but(p):
     'statement_list : statement SEP_BUT statement_list'
-    p[0] = StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_list_sep_then(p):
     'statement_list : statement SEP_THEN statement_list'
-    p[0] = StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_alicespoke(p):
     'statement : expression PRINT_SPOKE'
-    p[0] = SpokeNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.SpokeNode(p.lineno(1), p.clauseno(1), p[1])
 
 def p_statement_too(p):
     'statement : statement TOO'
@@ -65,11 +53,11 @@ def p_statement_too(p):
 
 def p_statement_wasa(p):
     'statement : ID DEC_WAS DEC_A type'
-    p[0] = DeclarationNode(p.lineno(1), p.clauseno(1), [p[1], p[4]])
+    p[0] = ASTNode.DeclarationNode(p.lineno(1), p.clauseno(1), [p[1], p[4]])
 
 def p_statement_became(p):
     'statement : ID ASSIGNMENT expression'
-    p[0] = AssignmentNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
+    p[0] = ASTNode.AssignmentNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 # Have to implement for drank and ate
 def p_statement_expression(p):
@@ -78,60 +66,60 @@ def p_statement_expression(p):
 
 def p_type_number(p):
     'type : TYPE_NUMBER'
-    p[0] = NumberTypeNode(p.lineno(1), p.clauseno(1))
+    p[0] = ASTNode.NumberTypeNode(p.lineno(1), p.clauseno(1))
 
 def p_type_letter(p):
     'type : TYPE_LETTER'
-    p[0] = LetterTypeNode(p.lineno(1), p.clauseno(1))
+    p[0] = ASTNode.LetterTypeNode(p.lineno(1), p.clauseno(1))
 
 def p_expression_not(p):
     'expression : B_NOT expression'
-    p[0] = UnaryNode(n.UNARY_OP, p.lineno(2), p.clauseno(2), p[1], p[2])
+    p[0] = ASTNode.UnaryNode(p.lineno(2), p.clauseno(2), p[1], p[2])
 
 def p_expression_drank(p):
     'expression : ID DECREMENT'
-    idNode = IDNode(p.lineno(1), p.clauseno(1), p[1])
-    p[0] = UnaryNode(n.UNARY_OP, p.lineno(2), p.clauseno(2), p[2], idNode)
+    idNode = ASTNode.IDNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.UnaryNode(p.lineno(2), p.clauseno(2), p[2], idNode)
 
 def p_expression_ate(p):
     'expression : ID INCREMENT'
-    idNode = IDNode(p.lineno(1), p.clauseno(1), p[1])
-    p[0] = UnaryNode(n.UNARY_OP, p.lineno(2), p.clauseno(2), p[2], idNode)
+    idNode = ASTNode.IDNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.UnaryNode(p.lineno(2), p.clauseno(2), p[2], idNode)
 
 def p_expression_or(p):
     'expression : expression B_OR expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_xor(p):
     'expression : expression B_XOR expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_and(p):
     'expression : expression B_AND expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
     
 def p_expression_plus(p):
     'expression : expression PLUS expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_minus(p):
     'expression : expression MINUS expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_multiply(p):
     'expression : expression MULTIPLY expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 #Handles division by 0 constant
 def p_expression_divide(p):
     'expression : expression DIVIDE expression'
     if p[3].children[1] == 0:
         raise e.DivisionByZeroException(p.lineno(1), p.clauseno(1))
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_mod(p):
     'expression : expression MOD expression'
-    p[0] = BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+    p[0] = ASTNode.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_factor(p):
     'expression : factor'
@@ -139,15 +127,15 @@ def p_expression_factor(p):
 
 def p_factor_number(p):
     'factor : NUMBER'
-    p[0] = NumberNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.NumberNode(p.lineno(1), p.clauseno(1), p[1])
 
 def p_factor_letter(p):
     'factor : LETTER'
-    p[0] = LetterNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.LetterNode(p.lineno(1), p.clauseno(1), p[1])
 
 def p_factor_id(p):
     'factor : ID'
-    p[0] = IDNode(p.lineno(1), p.clauseno(1), p[1])
+    p[0] = ASTNode.IDNode(p.lineno(1), p.clauseno(1), p[1])
 
     
 def p_error(p):
