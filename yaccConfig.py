ce789d4a5b3285de1f45bf5148f86b73d9502967
Peter Hamilton <peter@inspiredpixel.net>
2011-11-22 13:19:12 +0000

    Updated yacc config, contains some new rules although a lot of them are broken and need some thought to be implemented correctly

--- yaccConfig.py
+++ yaccConfig.py
@@ -33,9 +33,23 @@ def p_statement_list_sep(p):
                         | statement SEP_THEN statement_list'''
     p[0] = ASTNodes.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
-def p_statement_alicespoke(p):
-    'statement : expression PRINT_SPOKE'
+def p_statement_print(p):
+    '''statement    : expression PRINT_SPOKE
+                    | expression PRINT_SAID ALICE'''
     p[0] = ASTNodes.SpokeNode(p.lineno(1), p.clauseno(1), p[1])
+
+def p_statement_return(p):
+    'statement    : expression RETURN_FOUND'
+    p[0] = ASTNodes.ReturnNode(p.lineno(1), p.clauseno(1), p[1])
+
+def p_statement_input(p):
+    'statement : INPUT_WHAT ID'
+    p[0] = ASTNodes.InputNode(p.lineno(1), p.clauseno(1), p[2])
+
+# TODO: "WORK ME OUT" thought Pete....
+def p_statement_comment(p):
+    'statement : expression COMMENT_THOUGHT'
+    p[0] = ASTNodes.CommentNode(p.lineno(1), p.clauseno(1), p[1])
 
 def p_statement_too(p):
     'statement : statement TOO'
@@ -48,6 +62,52 @@ def p_statement_wasa(p):
 def p_statement_became(p):
     'statement : ID ASSIGNMENT expression'
     p[0] = ASTNodes.AssignmentNode(p.lineno(1), p.clauseno(1), p[1], p[3])
+
+#TODO: SHOULD WE ONLY MATCH ON NUMBERS e.g "a has 'a' number" readable but catch later?
+#TODO: Work out Node format
+def p_statement_array_has(p):
+    'statement : ID ARRAY_HAS expression type'
+    p[0] = ASTNodes.ArrayDeclarationNode(p.lineno(1), p.clauseno(1), p[2])
+ 
+#TODO: Work out Node format
+def p_statement_array_assign(p):
+    'statement : ID expression ARRAY_PIECE ASSIGNMENT expression'
+    p[0] = ASTNodes.ArrayAssignment(p.lineno(1), p.clauseno(1), p[2])
+
+#TODO: Work out Node format, should be logical expression?
+def p_statement_loop(p):
+    'statement : LOOP_EVENTUALLY expression LOOP_BECAUSE statement_list LOOP_ENOUGH LOOP_TIMES'
+    p[0] = ASTNodes.LoopNode(p.lineno(1), p.clauseno(1), p[2])
+
+#TODO: FIGURE OUT HOW IF STRUCTURE IS REPRESENTED
+def p_statement_if_general(p):
+    # if
+    'statement    : IF_PERHAPS expression_logical IF_SO statement_list ALICE WAS IF_UNSURE IF_WHICH'
+    
+    # else if
+    'statement    : IF_PERHAPS expression_logical IF_SO statement_list IF_OR IF_MAYBE expression_logical IF_SO statement_list ALICE WAS IF_UNSURE IF_WHICH'
+    
+    # else
+    'statement    : IF_PERHAPS expression_logical IF_SO statement_list IF_OR statement_list ALICE WAS IF_UNSURE IF_WHICH'
+
+def p_statement_if_either(p):
+    # if (either = 2 options only)
+    'statement    : IF_EITHER expression IF_SO statement_list ALICE WAS IF_UNSURE IF_WHICH'
+
+
+
+def p_statement_modifier_function(p):
+    'statement : FUNCTION_THE FUNCTION_LOOKING_GLASS ID FUNCTION_CHANGED DEC_A type'
+    #p[3] is the id of the function, p[6] is the type it takes
+    #Within function symbol 'it' refers to the passed ID
+        
+# Pass variable to modifier function
+def p_statement_pass_to_modifier_function(p):
+    'statement : ID FUNCTION_WENT FUNCTION_THROUGH ID'
+
+# Pass variable to modifier function
+def p_statement__function(p):
+'statement : FUNCTION_THE FUNCTION_ROOM ID (ARGUMENTS) FUNCTION_CONTAINED DEC_A type statement_list ALICE RETURN_FOUND expression'
 
 def p_statement_expression(p):
     'statement : expression'
@@ -78,6 +138,22 @@ def p_expression_binary(p):
                     | expression PLUS expression
                     | expression MINUS expression
                     | expression MULTIPLY expression'''
+    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
+
+#Not sure about this, might cause loads of reduce conflicts?
+def p_expression_expression_logical(p):
+    'expression : expression_logical'
+    p[0] = p[1]
+
+def p_expression_logical(p):
+    '''expression   : expression L_EQUALS expression
+                    | expression L_LESS_THAN expression
+                    | expression L_GREATER_THAN expression
+                    | expression L_GREATER_THAN_EQUAL expression
+                    | expression L_LESS_THAN_EQUAL expression
+                    | expression L_NOT_EQUAL expression
+                    | expression L_AND expression
+                    | expression L_OR expression'''
     p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 #TODO: MOVE THESE EXPRESSIONS INTO GENERAL BINARY EXPRESSION ABOVE AND MOVE
