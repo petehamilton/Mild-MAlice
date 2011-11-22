520b7087c9651f398ec7cfce4a36644bbacd48a5
Peter Hamilton <peter@inspiredpixel.net>
2011-11-22 09:53:31 +0000

    Combined similar yacc expressions together to reduce file size and make it more similar to normal BNF-style

--- yaccConfig.py
+++ yaccConfig.py
@@ -27,16 +27,10 @@ def p_statement_list_statement(p):
     'statement_list : statement SEP_PERIOD'
     p[0] = p[1]
 
-def p_statement_list_sep_and(p):
-    'statement_list : statement SEP_AND statement_list'
-    p[0] = ASTNodes.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
-
-def p_statement_list_sep_but(p):
-    'statement_list : statement SEP_BUT statement_list'
-    p[0] = ASTNodes.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
-
-def p_statement_list_sep_then(p):
-    'statement_list : statement SEP_THEN statement_list'
+def p_statement_list_sep(p):
+    '''statement_list   : statement SEP_AND statement_list
+                        | statement SEP_BUT statement_list
+                        | statement SEP_THEN statement_list'''
     p[0] = ASTNodes.StatementListNode(p.lineno(1), p.clauseno(1), [p[1], p[3]])
 
 def p_statement_alicespoke(p):
@@ -71,49 +65,29 @@ def p_expression_not(p):
     'expression : B_NOT expression'
     p[0] = ASTNodes.UnaryNode(p.lineno(2), p.clauseno(2), p[1], p[2])
 
-def p_expression_drank(p):
-    'expression : ID DECREMENT'
+def p_expression_inc_dec(p):
+    '''expression   : ID DECREMENT
+                    | ID INCREMENT'''
     idNode = ASTNodes.IDNode(p.lineno(1), p.clauseno(1), p[1])
     p[0] = ASTNodes.UnaryNode(p.lineno(2), p.clauseno(2), p[2], idNode)
 
-def p_expression_ate(p):
-    'expression : ID INCREMENT'
-    idNode = ASTNodes.IDNode(p.lineno(1), p.clauseno(1), p[1])
-    p[0] = ASTNodes.UnaryNode(p.lineno(2), p.clauseno(2), p[2], idNode)
-
-def p_expression_or(p):
-    'expression : expression B_OR expression'
+def p_expression_binary(p):
+    '''expression   : expression B_OR expression
+                    | expression B_XOR expression
+                    | expression B_AND expression
+                    | expression PLUS expression
+                    | expression MINUS expression
+                    | expression MULTIPLY expression
+                    | expression MOD expression'''
     p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
-def p_expression_xor(p):
-    'expression : expression B_XOR expression'
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-
-def p_expression_and(p):
-    'expression : expression B_AND expression'
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-    
-def p_expression_plus(p):
-    'expression : expression PLUS expression'
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-
-def p_expression_minus(p):
-    'expression : expression MINUS expression'
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-
-def p_expression_multiply(p):
-    'expression : expression MULTIPLY expression'
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-
-#Handles division by 0 constant
+#TODO: MOVE THESE EXPRESSIONS INTO GENERAL BINARY EXPRESSION ABOVE AND MOVE
+# DIV/0 CHECK INTO ASTNODE CHECK FUNCTION
 def p_expression_divide(p):
-    'expression : expression DIVIDE expression'
+    '''expression   : expression DIVIDE expression
+                    | expression MOD expression'''
     if p[3].getValue() == 0:
         raise e.DivisionByZeroException(p.lineno(1), p.clauseno(1))
-    p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
-
-def p_expression_mod(p):
-    'expression : expression MOD expression'
     p[0] = ASTNodes.BinaryNode(p.lineno(1), p.clauseno(1), p[2], [p[1],p[3]])
 
 def p_expression_factor(p):
