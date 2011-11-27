dfba6a9c4ee9b80d1323c28d6dd4810ccadacc38
Peter Hamilton <peter@inspiredpixel.net>
2011-11-27 16:45:35 +0000

    Sectioned out the exceptions file a bit in preparation for the new exceptions we are going to add

--- grammarExceptions.py
+++ grammarExceptions.py
@@ -2,17 +2,42 @@ class GrammarException(Exception):
     def __init__( self, lineno, clauseno ):
         self.lineno = lineno
         self.clauseno = clauseno
-        
+
+################################################################################
+# SEMANTIC EXCEPTIONS
+################################################################################
 class SemanticException(GrammarException):
     def __init__( self, lineno, clauseno, value = "" ):
         super(SemanticException, self).__init__(lineno, clauseno)       
         self.value = "Oh No! Silly you!" + value
-        
+
+class BinaryException(SemanticException):
+    def __init__( self, lineno, clauseno, value = "" ):
+        super(BinaryException, self).__init__(lineno, clauseno, value)
+
+class LogicalException(SemanticException):
+    def __init__( self, lineno, clauseno, value = "" ):
+        super(LogicalException, self).__init__(lineno, clauseno, value)
+
+class UnaryException(SemanticException):
+    def __init__( self, lineno, clauseno, value = "" ):
+        super(UnaryException, self).__init__(lineno, clauseno, value)
+
+
+
+################################################################################
+# SYNTAX EXCEPTIONS
+################################################################################
 class SyntaxException(GrammarException):
     def __init__( self, lineno, clauseno, value = "" ):
         super(SyntaxException, self).__init__(lineno, clauseno)        
         self.value = "Oh No! You started writing utter nonsense." + value
-       
+
+
+
+################################################################################
+# OTHER EXCEPTIONS
+################################################################################
 class DivisionByZeroException(GrammarException):
     def __init__( self, lineno, clauseno, value = "" ):
         super(DivisionByZeroException, self).__init__(lineno, clauseno)       
