57cac5d8815104c4c0d3036303e51b6ab10c5999
Peter Hamilton <peter@inspiredpixel.net>
2011-11-28 12:11:00 +0000

    Added bit of code to print out the exception class thrown, not just an error message|

--- grammarExceptions.py
+++ grammarExceptions.py
@@ -9,7 +9,7 @@ class GrammarException(Exception):
 class SemanticException(GrammarException):
     def __init__( self, lineno, clauseno, value = "" ):
         super(SemanticException, self).__init__(lineno, clauseno)       
-        self.value = "Oh No! Silly you!" + value
+        self.value = "Oh No! Silly you!" + value + " " + self.__class__.__name__
 
 class BinaryException(SemanticException):
     def __init__( self, lineno, clauseno, value = "" ):
